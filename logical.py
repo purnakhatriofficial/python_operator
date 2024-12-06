0770a5358d47a45807f13069d4f9c75c30079708
purna khatri <purnakhatri630@gmail.com>
2024-12-06 15:18:51 +0400

    Compression

---
+++ logical.py
@@ -0,0 +1,15 @@
+number_one = 10
+
+number_two= 3
+
+and_operaton = (number_one > number_two) and (number_one > 20)
+
+or_operation = (number_one < number_two)  or (number_one < 20)
+
+not_operation = not(number_one > number_two)
+
+print(f"And operation : (number_one > number_two) and (number_one >20) is {and_operaton} ")
+
+print(f"Or operation : (number_one < number_two) or (number_one < 20 ) is {or_operation} ")
+
+print(f"NOT operation :  not(number_one > number_tqo) is {not_operation}")

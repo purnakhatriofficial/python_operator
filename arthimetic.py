e51f9d666e3ca16d58bcdba4ecd8b4a0fe081fd3
purna khatri <purnakhatri630@gmail.com>
2024-12-06 15:08:37 +0400

    Pythone_operator

---
+++ arthimetic.py
@@ -0,0 +1,18 @@
+number_one = int(input("Enter the your first number : "))
+number_two =  int(input("Enter the your second number :"))
+
+addition = (number_one) + (number_two)
+subtraction = (number_one) - (number_two)
+multiplication = (number_one) * (number_two)
+division = (number_one) / (number_two)
+modulations = (number_one) * (number_two) /100
+reminder = (number_one) // (number_two)
+square = (number_one)*(number_two)
+
+print(f"The addition number is : {addition}")
+print(f"The subtraction is : {subtraction}")
+print(f"The multiplication number is  : {multiplication}")
+print(f"The Division number is : {division}")
+print(f"The modulation number is : {modulations}")
+print(f"The reminder number is : {reminder}")
+print(f"The square number is : {square}")
---
+++ compression.py
@@ -0,0 +1,59 @@
+number_one = 20
+number_two = 10
+
+equal_number = number_one == number_two
+
+not_equal_number = number_one != number_two
+
+greater_than_number = number_one > number_two
+
+less_than_number = number_one < number_two
+
+greater_or_equal_number = number_one >= number_two
+
+less_or_equal = number_one <= number_two
+
+
+print(f" Equal : {number_one} == {number_two} is {equal_number}")
+
+print(f"Not equal : {number_one} != {number_two} is {not_equal_number}")
+
+print(f"Greater Number  : {number_one} > {number_two} is {greater_than_number}")
+
+print(f"Less than number : {number_one} < {number_two} is {less_than_number}")
+
+print(f"Greater or equal number  : {number_one} >= {number_two} is {greater_or_equal_number} ")
+
+print(f"Less or Equal number : {number_one} <= {number_two} is {less_or_equal}")
+
+print("\n")
+
+#User inputes :
+
+first_number = int(input("Enter the your first number : "))
+second_number = int(input ("Enter the second number : "))
+
+equal_number = first_number == second_number
+
+not_equal_number = first_number != second_number
+
+greater_than_number = first_number > second_number
+
+less_than_number = first_number < second_number
+
+greater_or_equal_number = first_number>= second_number
+
+less_or_equal = first_number<= second_number
+
+
+print(f" Equal : {first_number} == {second_number} is {equal_number}")
+
+print(f"Not equal : {first_number} != {second_number} is {not_equal_number}")
+
+print(f"Greater Number  : {first_number} > {second_number} is {greater_than_number}")
+
+print(f"Less than number : {first_number} < {second_number} is {less_than_number}")
+
+print(f"Greater or equal number  : {first_number} >= {second_number} is {greater_or_equal_number} ")
+
+print(f"Less or Equal number : {first_number} <= {second_number} is {less_or_equal}")
